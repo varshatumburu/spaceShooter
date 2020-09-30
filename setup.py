5ab8fdf46ee02eb96752adf0e0b9328140e35db4
Shashwat Mahajan <shashwat211@gmail.com>
2020-10-01 00:24:03 +0530

    An update on fixes

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ pygame_py_file = os.path.join('spaceshooter', 'spaceShooter.py')
 ## The image and sound files are added manually into the zip file
 ## A fix for this would be released
 ## Fix not released yet. Please wait.
+## Fix to be released within a month.
 
 setup(  name = "Space Shooter",
         version = "0.0.2",
