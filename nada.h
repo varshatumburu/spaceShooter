7cb962081275501a15e8eaa6c157073c6b4fe0f2
Balbeer Yadav <balbeery3099@gmail.com>
2021-10-03 15:07:39 +0530

    Update nada.h
    endl is not stable on some platforms. Used escape sequence instead.

--- nada.h
+++ nada.h
@@ -26,7 +26,7 @@ class Student {
     void printSubjects() {
         for (auto x: subjects) {
             
-            cout << x << endl;
+            cout << x << "\n";
             
         }
     }
