eddcbdbe59b0e699fecca23736035f92e1126316
Shashwat Mahajan <shashwat211@gmail.com>
2022-10-01 22:26:43 +0530

    Added working domain for student roll calls

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #define INIT_ROLL 2048
 #include <cstdlib>
+#include "nada.h"
 using namespace std;
 
 
@@ -15,4 +16,7 @@ int main()
     // rand()%6 generates number between 0 to 5
     for(int i=1;i<=25;i++)
     cout<<1 + rand()%6<<"\n";
+    
+    Student sde = Student();
+    cout << call_on_roll(sde.getroll()) << endl;  // Make a successful call on student roll
 }
